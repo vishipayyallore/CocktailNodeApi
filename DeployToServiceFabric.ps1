95bbaddee7180f512e4b8aef308825fe053650be
vishipayyallore <vishipayyallore@yahoo.com>
2016-02-22 20:44:08 +0530

    AA

---
+++ DeployToServiceFabric.ps1
@@ -0,0 +1,16 @@
+﻿#Package the Node JS application
+# .\ServiceFabricAppPackageUtil.exe /source:'A:\LordKrishna\Samples_Github\CocktailNodeApi' /target:'A:\LordKrishna\Samples_Github\Pre-Release-SFPackagingTool\Outputs\CocktailNodeApi' /appname:NodeService /exe:'node.exe' /ma:'bin/www' /AppType:NodeAppType
+
+
+#Connect to Local Service Fabric Cluster
+Connect-ServiceFabricCluster localhost:19000
+
+#Deploy to Local Service Fabric Cluster
+Write-Host 'Copying application package...'
+Copy-ServiceFabricApplicationPackage -ApplicationPackagePath 'A:\LordKrishna\Samples_Github\Pre-Release-SFPackagingTool\Outputs\CocktailNodeApi' -ImageStoreConnectionString 'file:C:\SfDevCluster\Data\ImageStoreShare' -ApplicationPackagePathInImageStore 'Store\NodeAppType'
+
+Write-Host 'Registering application type...'
+Register-ServiceFabricApplicationType -ApplicationPathInImageStore 'Store\NodeAppType'
+
+New-ServiceFabricApplication -ApplicationName 'fabric:/NodeApp' -ApplicationTypeName 'NodeAppType' -ApplicationTypeVersion 1.0  
+
--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('default', function(){
         script: 'application.js',
         ext: 'js',
         env: {
-            PORT: 8090,
+            PORT: 8091,
             ENV: 'development',
             MONGO_URI: "mongodb://localhost/cocktaildev"
         },
