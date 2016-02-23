9814887f3ff06832f577e847bdbaec91eb22bcc9
vishipayyallore <vishipayyallore@yahoo.com>
2016-02-23 22:10:02 +0530

    A

--- application.js
+++ application.js
@@ -19,15 +19,15 @@ application.use(bodyParser.urlencoded({
 }));
 
 application.get('/', function(req, res){
-    res.send('Welcome to Cocktail NodeJS API! ');
+    res.send('Welcome to Cocktail NodeJS API! ' + process.env.ENV);
 });
 
 application.get('/api', function(req, res){
-    res.send('Welcome to Cocktail NodeJS API! ');
+    res.send('Welcome to Cocktail NodeJS API! ' + process.env.ENV);
 });
 
 application.get('/api/HealthCheck', function(req, res){
-    res.send('Cocktail NodeJS API -> Health Check is Good! ');
+    res.send('Cocktail NodeJS API -> Health Check is Good! ' + process.env.ENV);
 });
 
 var Product = require('./models/product');
@@ -36,7 +36,7 @@ application.use('/api/products', productRouter);
 
 var portNumber = config.get('env.PORT') || 8091;
 var server = application.listen(portNumber, function() {
-    console.log('Server running at http://127.0.0.1:'+ portNumber + '/ in Azure (' + process.env.ENV + process.env.MONGO_URI + ') environment' );
+    console.log('Server running at http://127.0.0.1:'+ portNumber + '/ in Azure (' + process.env.ENV + ' ' + mongoConnectionString + ') environment' );
 });
 
 module.exports = application;
