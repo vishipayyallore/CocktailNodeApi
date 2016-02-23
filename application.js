57657adfc9a13f135c34c93736e0f0bfb22f4477
vishipayyallore <vishipayyallore@yahoo.com>
2016-02-23 22:23:08 +0530

    A

--- application.js
+++ application.js
@@ -18,16 +18,12 @@ application.use(bodyParser.urlencoded({
     extended: true
 }));
 
-application.get('/', function(req, res){
-    res.send('Welcome to Cocktail NodeJS API! ' + process.env.ENV);
-});
-
 application.get('/api', function(req, res){
-    res.send('Welcome to Cocktail NodeJS API! ' + process.env.ENV);
+    res.send('Welcome to Cocktail NodeJS API! ');
 });
 
 application.get('/api/HealthCheck', function(req, res){
-    res.send('Cocktail NodeJS API -> Health Check is Good! ' + process.env.ENV);
+    res.send('Cocktail NodeJS API -> Health Check is Good! ');
 });
 
 var Product = require('./models/product');
