111f4cf560d2cee5558e09601aecbd4d3f920cf3
vishipayyallore <vishipayyallore@yahoo.com>
2016-02-26 01:01:50 +0530

    Final Test

--- application.js
+++ application.js
@@ -30,7 +30,8 @@ var Product = require('./models/product');
 var productRouter = require('./routes/productRoutes')(Product);
 application.use('/api/products', productRouter);
 
-var portNumber = process.env.PORT || 8094;
+//var portNumber = process.env.PORT || 8094;
+var portNumber = config.get('env.PORT') || 8094;
 var server = application.listen(portNumber, function() {
     console.log('Server running ...' );
 });
