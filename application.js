86c70cfd38e3bb8ac726a5ffe1b3e45586500666
vishipayyallore <vishipayyallore@yahoo.com>
2016-02-26 00:43:25 +0530

    Removed unwanted stuff.

--- application.js
+++ application.js
@@ -9,7 +9,7 @@ var config = require('config');
 var mongoConnectionString = config.get('env.MONGO_URI');
 console.log( mongoConnectionString );
 //Mongo Database
-//var mongoDataStore = mongoose.connect(mongoConnectionString);
+var mongoDataStore = mongoose.connect(mongoConnectionString);
 
 var application = express();
 
@@ -26,15 +26,13 @@ application.get('/api/HealthCheck', function(req, res){
     res.send('Cocktail NodeJS API -> (Boris) Health Check is Good! ');
 });
 
-/*
 var Product = require('./models/product');
 var productRouter = require('./routes/productRoutes')(Product);
 application.use('/api/products', productRouter);
-*/
 
-var portNumber = config.get('env.PORT') || 8094;
+var portNumber = config.get('env.PORT');
 var server = application.listen(portNumber, function() {
-    console.log('Server running at http://127.0.0.1:'+ portNumber + '/ in Azure (' + process.env.ENV + ' ' + mongoConnectionString + ') environment' );
+    console.log('Server running ...' );
 });
 
 module.exports = application;
